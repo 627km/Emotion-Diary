43ba9302f3ee2654914e5c34b828d1be9cef7347
627km <rudals_jeon@naver.com>
2023-08-02 16:16:52 +0900

    feat: Setting
    fonts, layout, image assets, common components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import RouteTest from "./components/RouteTest";
-
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
+// COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
 function App() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton
+              text={"왼쪽버튼"}
+              onClick={() => {
+                alert("왼쪽클릭");
+              }}
+            />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽버튼"}
+              onClick={() => {
+                alert("오른쪽클릭");
+              }}
+            />
+          }
+        />
         <h2>App.js</h2>
+        {/* process.env.PUBLIC_URL : 현재 파일이 어디있든 상관없이 바로 public 디렉토리를 가리키게 된다 */}
+        {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} /> */}
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"negative"}
+        />
+        <MyButton text={"버튼"} onClick={() => alert("버튼클릭")} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest></RouteTest>
       </div>
     </BrowserRouter>
   );
