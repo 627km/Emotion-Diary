408ae11ad681a39ecd969f571a6c6d06f9a8bf6b
627km <rudals_jeon@naver.com>
2023-08-07 17:54:59 +0900

    feat: Use localStorage

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -13,7 +13,7 @@ const DiaryEditor = ({ isEdit, originData }) => {
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
 
-  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
   const navigate = useNavigate();
 
   const handleSubmit = () => {
@@ -34,6 +34,14 @@ const DiaryEditor = ({ isEdit, originData }) => {
     }
     navigate("/", { replace: true });
   };
+
+  const handleRemove = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onRemove(originData.id);
+      navigate("/", { replace: true });
+    }
+  };
+
   const handleClickEmote = (emotion) => {
     setEmotion(emotion);
   };
@@ -51,6 +59,15 @@ const DiaryEditor = ({ isEdit, originData }) => {
         headText={isEdit ? "일기 수정하기" : "새로운 일기 쓰기"}
         leftChild={
           <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+        rightChild={
+          isEdit && (
+            <MyButton
+              text={"삭제하기"}
+              type={"negative"}
+              onClick={handleRemove}
+            />
+          )
         }
       />
       <div>
